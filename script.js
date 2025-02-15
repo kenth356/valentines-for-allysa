4008655d6126ad8c7bf22aa5c5264c9c2a5f960a
Kenth Mariano <kjsamonte27@gmail.com>
2025-02-15 11:53:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function moveRandomEl(elm) {
+    elm.style.position = "absolute";
+    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+}
+
+const moveRandom = document.querySelector("#move");
+move.addEventListener("mouseenter", function (e) {
+    moveRandomEl(e.target);
+});
